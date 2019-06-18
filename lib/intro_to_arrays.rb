2d9036340ae58599ed626a0f979f69405aa15c49
Jordan Bacon <jmbacon92@gmail.com>
2019-06-18 08:57:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,3 +17,11 @@ end
 def last_element(array)
   return array[-1]
 end
+
+def first_element_with_array_methods(array)
+  return array.shift
+end
+
+def first_element_with_array_methods(array)
+  return array.pop
+end
